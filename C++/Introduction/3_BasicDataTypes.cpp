2007aa0d5042a88729d6f506fc8510b43064a3f9
AmirWS <113177909+AmirWS@users.noreply.github.com>
2023-11-10 02:49:55 +0200

    Create 3_BasicDataTypes.cpp

---
+++ C++/Introduction/3_BasicDataTypes.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int x1;long x2; char x3; float x4; double x5;
+
+    cin>>x1>>x2>>x3>>x4>>x5;
+    cout<<x1<<endl;
+    cout<<x2<<endl;
+    cout<<x3<<endl;
+    cout<<fixed<<setprecision(3)<<x4<<endl;
+    cout<<fixed<<setprecision(5)<<x5<<endl;
+    
+    
+
+    return 0;
+}
